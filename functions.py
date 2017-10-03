d52e99aa1cd9a578354beb868c38fe1a7e5b395d
Aleksandr Titov <t_aleksandr_v@inbox.ru>
2017-10-03 12:53:46 +0300

    Open file use "with"

--- functions.py
+++ functions.py
@@ -9,14 +9,16 @@ import json
 
 def count_words(file_name):
     dict_words = {}
-    for string in open(file_name):
-        words = (re.sub('\W', ' ', string.lower()).split())
+    with open(file_name) as dict_of_words:
+        text = re.sub('\W', ' ', dict_of_words.read())
+        words = text.lower().split()
         for word in words:
-            if word in dict_words:
-                dict_words[word][0] += 1
-            else:
-                param = [1,""]
-                dict_words[word] = param
+            if len(word) > 3:
+                if word in dict_words:
+                    dict_words[word][0] += 1
+                else:
+                    param = [1,""]
+                    dict_words[word] = param
     return dict_words
 
 
@@ -27,15 +29,14 @@ def count_words(file_name):
 
 def translate_words(dict_words, token):
     for word in dict_words:
-        if len(word) > 3:
-            link = 'https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=' + token + '&lang=en-ru&text=' + word + '&flags=4'
-            response = requests.get(link, timeout=10)
-            translate = json.loads(response.text)
-            try:
-                word_tr = translate['def'][0]['tr'][0]['text']
-            except IndexError:
-                word_tr = "none"
-            dict_words[word][1] = word_tr
+        link = 'https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=' + token + '&lang=en-ru&text=' + word + '&flags=4'
+        response = requests.get(link, timeout=10)
+        translate = json.loads(response.text)
+        try:
+            word_tr = translate['def'][0]['tr'][0]['text']
+        except IndexError:
+            word_tr = "none"
+        dict_words[word][1] = word_tr
     return dict_words
 
 
--- translator.py
+++ translator.py
@@ -7,5 +7,4 @@ file = "FILE WITH ENGLISH TEXT"
 all_worlds = functions.sort_words(functions.translate_words(functions.count_words(file), token))
 
 for i in all_worlds:
-    if len(i[0]) > 3:
-        print(i[1][0], ": ", i[0], i[1][1])
+    print(i[1][0], ": ", i[0], i[1][1])
